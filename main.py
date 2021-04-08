0a5168d86fea46bc29f3e02301c215b74cc3aa41
Deimer Estiven Estiven <>
2021-04-08 05:05:49 +0000

    ya

--- main.py
+++ main.py
@@ -30,22 +30,25 @@ horas=openpyxl.load_workbook('Minuevoarchivo.xlsx')
 
 columna=horas.active
 
+#Estoy guardando el momento de inicio
 inicio = datetime.datetime.now()
 
+#inserto dos fechas y le calculo la diferencia, por sierto esa diferencia estará en horas
 hoy=dt.datetime(2021,4,6,)
 micumple = dt.datetime(1999,5,10)
 diferencia=hoy - micumple
 
-
+#Inserto esa diferencia en la columna D del documento de excel
 columna['D1']='Diferencia de fechas'
 for i in range(2,5):
   columna[f'D{i}']=diferencia
 
-
+#Aquí guardo el momento en que termina
 fin = datetime.datetime.now()
 
 duracion = fin - inicio
 
+#concateno dos celdas y luego inserto los datos de inicio, fin y tiempo que duro la ejecución de los algoritmos anteriores
 columna.merge_cells('E1:F1')
 columna['E1']='tiempo de ejecución de fechas'
 columna['E2']='Inicio'
@@ -55,9 +58,6 @@ columna['F3']=fin
 columna['E4']='Duración'
 columna['F4']=duracion
 
-
-
+#Guardo todo en un nuevo documento y listo
 horas.save('Ejecucion_tiempo.xlsx')
-
-print('Se ha ejecutado')
 
